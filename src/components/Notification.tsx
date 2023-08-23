087673caacf5cbf78cfe1a84db5b07930f58423e
jay-rt <thapa.jayrt@gmail.com>
2023-08-23 17:20:44 -0400

    Created base pages and components and finished notification component

---
+++ src/components/Notification.tsx
@@ -0,0 +1,9 @@
+const Notification = () => {
+  return (
+    <div className="h-12 bg-red-500 text-white flex-center text-center text-sm md:text-base cursor-pointer px-4">
+      Free delivery for all orders over $50. Order your food now!
+    </div>
+  );
+};
+
+export default Notification;
