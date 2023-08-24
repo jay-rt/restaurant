90e8e85b67e6e31e9404021db7b38d543e7b9372
jay-rt <thapa.jayrt@gmail.com>
2023-08-23 22:18:35 -0400

    finished footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="h-12 md:h-24 flex-between text-red-500 p-4 lg:p-20 xl:p-40">
+      <Link href="/" className="font-bold text-xl">
+        MASSIMO
+      </Link>
+      <span>&copy; ALL RIGHTS RESERVED</span>
+    </div>
+  );
 };
 
 export default Footer;
